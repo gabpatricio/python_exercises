93abe299038b8932f26db480045502dc52f348d7
Gabriela S Patricio <104843522+gabpatricio@users.noreply.github.com>
2022-09-27 14:12:35 -0300

    Create str.py

---
+++ str.py
@@ -0,0 +1,53 @@
+#Tamanho de strings. Faça um programa que leia 2 strings e informe o conteúdo delas seguido do seu comprimento. Informe também se as duas strings possuem o mesmo comprimento e são iguais ou diferentes no conteúdo.
+
+a = input("Digite a primeira palavra: ").split()
+b = input("Digite a segunda palavra: ").split()
+
+c = len(a)
+d = len(b)
+
+print("A primeira palavra é: ", a)
+print("A segunda palavra é: ", b)
+print("A primeira palavra tem ", c, "caracteres")
+print("A segunda palavra tem ", d, "caracteres")
+
+
+
+#Nome ao contrário em maiúsculas. Faça um programa que permita ao usuário digitar o seu nome e em seguida mostre o nome do usuário de trás para frente utilizando somente letras maiúsculas. Dica: lembre−se que ao informar o nome o usuário pode digitar letras maiúsculas ou minúsculas.
+
+#Nome na vertical
+
+#Nome na vertical em escada
+
+#Nome na vertical em escada invertida
+
+#Data por extenso. Faça um programa que solicite a data de nascimento (dd/mm/aaaa) do usuário e imprima a data com o nome do mês por extenso.
+
+#Conta espaço e vogais
+
+#Verificação de CPF. Desenvolva um programa que solicite a digitação de um número de CPF no formato xxx.xxx.xxx-xx e indique se é um número válido ou inválido através da validação dos dígitos verificadores edos caracteres de formatação.
+
+#Número por extenso. Escreva um programa que solicite ao usuário a digitação de um número até 99 e imprima-o na tela por extenso.
+
+#Jogo de Forca. Desenvolva um jogo da forca. O programa terá uma lista de palavras lidas de um arquivo texto e escolherá uma aleatoriamente. O jogador poderá errar 6 vezes antes de ser enforcado.
+#Digite uma letra: A
+#-> Você errou pela 1ª vez. Tente de novo!
+#Digite uma letra: O
+#A palavra é: _ _ _ _ O
+#Digite uma letra: E
+#A palavra é: _ E _ _ O
+#Digite uma letra: S
+#-> Você errou pela 2ª vez. Tente de novo!
+
+#Valida e corrige número de telefone. Faça um programa que leia um número de telefone, e corrija o número no caso deste conter somente 7 dígitos, acrescentando o '3' na frente. O usuário pode informar o número com ou sem o traço separador.
+
+#Valida e corrige número de telefone
+#Telefone: 461-0133
+#Telefone possui 7 dígitos. Vou acrescentar o digito três na frente.
+#Telefone corrigido sem formatação: 34610133
+#Telefone corrigido com formatação: 3461-0133
+
+#Jogo da palavra embaralhada. Desenvolva um jogo em que o usuário tenha que adivinhar uma palavra que será mostrada com as letras embaralhadas. O programa terá uma lista de palavras lidas de um arquivo texto e escolherá uma aleatoriamente. O jogador terá seis tentativas para adivinhar a palavra. Ao final a palavra deve ser mostrada na tela, informando se o usuário ganhou ou perdeu o jogo.
+
+#Leet spek generator. Leet é uma forma de se escrever o alfabeto latino usando outros símbolos em lugar das letras, como números por exemplo. A própria palavra leet admite muitas variações, como l33t ou 1337. O uso do leet reflete uma subcultura relacionada ao mundo dos jogos de computador e internet, sendo muito usada para confundir os iniciantes e afirmar-se como parte de um grupo. Pesquise sobre as principais formas de traduzir as letras. Depois, faça um programa que peça uma texto e transforme-o para a grafia leet speak.
+
